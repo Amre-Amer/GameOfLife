002b60e6e6251128417ad71e37824dfbfba307a7
Amre <me@amre-amer.com>
2018-02-18 09:25:04 -0800

    GOL 2d 3d initial commit

---
+++ Assets/GOL.cs
@@ -0,0 +1,275 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOL : MonoBehaviour {
+    public int resX = 100;
+    public int resY = 50;
+    public GameObject[,] golGOs;
+    public bool[,] grid;
+    public bool[,] gridNew;
+    public bool[,] gridOld;
+    public bool ynShowNeighbors;
+    [Header(" ")]
+    public bool ynShowNew;
+    public bool ynShowAlone;
+    public bool ynShowCrowded;
+    [Header(" ")]
+    public bool ynGlide;
+    public bool yn3D;
+    [Header(" ")]
+    public float elapsedTime;
+    public int total;
+    public float density;
+    public bool ynStep;
+    public bool ynRestart;
+    GameObject parent;
+    bool ynGlideLast;
+    float tStepStart;
+    int maxCrowdX;
+    int maxCrowdY;
+    int maxCrowd;
+    [Range(1, 50)]
+    public float cameraDist = 5;
+
+    void Update () {
+        if (ynStep == true)
+        {
+            if (Time.realtimeSinceStartup - tStepStart > 1) {
+                tStepStart = Time.realtimeSinceStartup;
+                regenGrid();
+            }
+        } else {
+            regenGrid();
+        }
+        followCrowded();
+        checkRestart();
+	}
+
+    void followCrowded() {
+        Vector3 pos = new Vector3(resX / 2, 0, resY / 2);
+        if (ynGlide == true) {
+            pos = new Vector3(maxCrowdX, 0, maxCrowdY);
+        }
+        Camera.main.transform.position = new Vector3(pos.x + cameraDist, cameraDist * 2, pos.y);
+        Camera.main.transform.LookAt(pos);
+    }
+
+    void checkRestart() {
+        if (ynGlide != ynGlideLast)
+        {
+            ynRestart = true;
+        }
+        ynGlideLast = ynGlide;
+    }
+
+    void regenGrid() {
+        float t = Time.realtimeSinceStartup;
+        if (golGOs == null) {
+            golGOs = new GameObject[resX, resY];
+            initGrid();
+        } else {
+            if (ynRestart == true)
+                {
+                ynRestart = false;
+                initGrid();
+                System.Array.Clear(gridNew, 0, gridNew.Length);
+            }
+            int cnt = 0;
+            for (int x = 0; x < resX; x++)
+            {
+                for (int y = 0; y < resY; y++)
+                {
+                    bool ynAlive = getGolState(x, y);
+                    if (ynAlive == true) {
+                        cnt++;                        
+                    }
+                    gridNew[x, y] = ynAlive;
+                }
+            }
+            total = resY * resY;
+            density = cnt / (float)total;
+            System.Array.Copy(grid, gridOld, gridOld.Length);
+            System.Array.Copy(gridNew, grid, grid.Length);
+            System.Array.Clear(gridNew,0, gridNew.Length);
+        }
+        drawGrid();
+        elapsedTime = Time.realtimeSinceStartup - t;
+    }
+
+    void drawGrid()
+    {
+        for (int x = 0; x < resX; x++)
+        {
+            for (int y = 0; y < resY; y++)
+            {
+                setGo(x, y);
+                setGolColor(x, y);
+                setGolScale(x, y);
+            }
+        }
+    }
+
+    void setGolScale(int x, int y) {
+        GameObject go = golGOs[x, y];
+        float sx = .9f;
+        float sy = .9f;
+        float sz = .9f;
+        Vector3 pos = go.transform.position;
+        pos.y = 0;
+        if (ynShowNeighbors == true)
+        {
+            sy *= getNeighborCount(x, y);
+            pos.y = sy / 2;
+        }
+        go.transform.localScale = new Vector3(sx, sy, sz);
+        go.transform.position = pos;
+    }
+
+    void setGolColor(int x, int y)
+    {
+        Color col = Color.black;
+        bool ynAlive = grid[x, y];
+        if (ynAlive == true)
+        {
+            col = Color.black;
+                if (ynShowNew == true)
+            {
+                if (gridOld[x, y] == false)
+                {
+                    col = Color.magenta;
+                }
+            }
+            if (ynShowAlone == true)
+            {
+                if (getNeighborCount(x, y) == 1)
+                {
+                    col = Color.cyan;
+                }
+            }
+            if (ynShowCrowded == true)
+            {
+                if (getNeighborCount(x, y) > 3)
+                {
+                    col = Color.red;
+                }
+            }
+        } else {
+            col = Color.white;
+        }
+        GameObject go = golGOs[x, y];
+        setColor(go, col);
+    }
+
+    void setGo(int x, int y)
+    {
+        GameObject go = golGOs[x, y];
+        if (go == null)
+        {
+            if (parent == null)
+            {
+                parent = new GameObject("parent");
+                parent.transform.parent = transform;
+            }
+            go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            go.transform.parent = parent.transform;
+            go.name = "lod " + x + " " + y;
+            Vector3 pos = new Vector3(x, 0, y);
+            go.transform.position = pos;
+            golGOs[x, y] = go;
+        }
+    }
+
+    void initGrid() {
+        grid = new bool[resX, resY];
+        gridNew = new bool[resX, resY];
+        gridOld = new bool[resX, resY];
+        if (ynGlide == true)
+        {
+            initGridGlider();
+        }
+        else
+        {
+            initGridRandom();
+        }
+    }
+
+    void initGridRandom() {
+        for (int x = 0; x < resX; x++)
+        {
+            for (int y = 0; y < resY; y++)
+            {
+                setGrid(x, y, getRandomBool());
+            }
+        }
+    }
+
+    void initGridGlider() {
+        System.Array.Clear(grid, 0, grid.Length);
+        setGrid(1, 0, true);
+        setGrid(2, 1, true);
+        setGrid(0, 2, true);
+        setGrid(1, 2, true);
+        setGrid(2, 2, true);
+    }
+
+    void setGrid(int x, int y, bool yn) {
+        grid[x, y] = yn;
+   }
+
+    bool getRandomBool() {
+        return (Random.value > 0.5f);
+    }
+
+    bool getGolState(int x, int y) {
+        int cnt = getNeighborCount(x, y);
+        bool ynAlive = grid[x, y];
+        if (ynAlive == true) {
+            if (cnt < 2 || cnt > 3) {
+                ynAlive = false;                
+            }            
+        } else {
+            if (cnt == 3) {
+                ynAlive = true;                
+            }
+        }
+        if (ynGlide == true) {
+            if (x == 0 && y == 0)
+            {
+                maxCrowd = -1;
+            }
+            if (cnt > 3) {
+                if (maxCrowd == -1 || cnt > maxCrowd)
+                {
+                    maxCrowdX = x;
+                    maxCrowdY = y;
+                    maxCrowd = cnt;
+                }
+            }
+        }
+        return ynAlive;
+    }
+
+    int getNeighborCount(int x, int y) {
+        int cnt = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++)
+            {
+                int xi = (x + i + resX) % resX;
+                int yj = (y + j + resY) % resY;
+                if (grid[xi, yj] == true) {
+                    cnt++;
+                }
+            }
+        }
+        if (grid[x, y] == true) {
+            cnt--;
+        }
+        return cnt;
+    }
+
+    void setColor(GameObject go, Color col) {
+        Renderer rend = go.GetComponent<Renderer>();
+        rend.material.color = col;
+    }
+}
---
+++ Assets/GOL3D.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOL3D : MonoBehaviour {
+    public int resX = 12;
+    public int resY = 12;
+    public int resZ = 12;
+    public GameObject[,,] gol3DGOs;
+    GameObject parent;
+    [Range(0, 27)]
+    public int numAlone = 5; 
+    [Range(0, 27)]
+    public int numCrowded = 7;
+    [Range(0, 27)]
+    public int numNew = 2;
+    public float elapsedTime;
+    public int total;
+    public float density;
+    public bool[,,] grid3D;
+    public bool[,,] grid3Dnew;
+    public bool[,,] grid3Dold;
+    public bool ynReset;
+    float startStepTime;
+    public bool ynStep;
+
+	// Use this for initialization
+	void Start () {
+        total = resX * resY * resZ;
+//        ynReset = true;
+//        UpdateX();
+//        UpdateX();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ynStep == true) {
+            if (Time.realtimeSinceStartup - startStepTime > 1)
+            {
+                startStepTime = Time.realtimeSinceStartup;
+                updateGrid3D();
+            }
+        } else {
+            updateGrid3D();
+        }
+	}
+
+    void updateGrid3D() {
+        Debug.Log("updateGrid3D");
+        float t = Time.realtimeSinceStartup;
+        regenGrid3Dnew();
+        drawGrid3D();
+        elapsedTime = Time.realtimeSinceStartup - t;
+    }
+
+    void initGrid3D() {
+        Debug.Log("initGrid");
+        initGrid3Drandom();
+    }
+
+    void initGrid3Drandom() {
+        for (int x = 0; x < resX; x++)
+        {
+            for (int y = 0; y < resY; y++)
+            {
+                for (int z = 0; z < resZ; z++)
+                {
+                    grid3D[x, y, z] = getRandomBool();
+                }
+            }
+        }
+    }
+
+    void regenGrid3Dnew() {
+        if (grid3D == null) {
+            grid3D = new bool[resX, resY, resZ];
+            grid3Dnew = new bool[resX, resY, resZ];
+            grid3Dold = new bool[resX, resY, resZ];
+            initGrid3D();
+            Debug.Log("1");
+            return;
+        }
+        if (ynReset == true) {
+            ynReset = false;
+            initGrid3D();
+        }
+        int cntAlive = 0;
+        for (int x = 0; x < resX; x++)
+        {
+            for (int y = 0; y < resY; y++)
+            {
+                for (int z = 0; z < resZ; z++)
+                {
+                    int cnt = getNeighbor3DCount(x, y, z);
+                    bool ynAlive = grid3D[x, y, z];
+                    if (ynAlive == true) {
+                        if (cnt < numAlone || cnt > numCrowded) {
+                            ynAlive = false;
+                        }
+                    } else {
+                        if (cnt == numNew)
+                        {
+                            ynAlive = true;
+                        }
+                    }
+                    if (ynAlive == true) {
+                        cntAlive++;
+                    }
+                    grid3Dnew[x, y, z] = ynAlive;
+                }
+            }
+        }
+        density = cntAlive / (float)(resX * resY * resZ);
+        System.Array.Copy(grid3Dnew, grid3D, grid3D.Length);
+    }
+
+    int getNeighbor3DCount(int x, int y , int z) {
+        int cnt = 0;
+        for (int i = -1; i <= 1; i++) 
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                for (int k = -1; k <= 1; k++)
+                {
+                    int ii = x + i;
+                    int jj = y + j;
+                    int kk = z + k;
+                    ii = (ii + resX) % resX;
+                    jj = (jj + resY) % resY;
+                    kk = (kk + resZ) % resZ;
+                    if (grid3D[ii, jj, kk] == true) {
+                        cnt++;                        
+                    }
+                }
+            }
+            if (grid3D[x,y,z] == true) {
+                cnt--;
+            }
+        }
+        return cnt;
+    }
+
+    void drawGrid3D() {
+        if (gol3DGOs == null) {
+            parent = new GameObject("parent");
+            parent.transform.parent = transform;
+            gol3DGOs = new GameObject[resX, resY, resZ];
+        }
+        for (int x = 0; x < resX; x++) 
+        {
+            for (int y = 0; y < resY; y++)
+            {
+                for (int z = 0; z < resZ; z++)
+                {
+                    GameObject go = gol3DGOs[x, y, z];
+                    if (go == null) {
+                        go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        go.transform.parent = parent.transform;
+                        go.name = "lod3D " + x + " " + y + " " + z;
+                        go.transform.position = new Vector3(x, y, z);
+                        float s = .9f;
+                        go.transform.localScale = new Vector3(s, s, s);
+                        gol3DGOs[x, y, z] = go; 
+                    }
+                    if (grid3D[x, y, z] == true)
+                    {
+                        setColor(go, new Color(1, 0, 0, 1));
+                    }
+                    else
+                    {
+                        setColor(go, new Color(.5f, .5f, .5f, 0.125f));
+                    }
+                }
+            }
+        }
+    }
+
+    void setColor(GameObject go, Color col) {
+        Renderer rend = go.GetComponent<Renderer>();
+        rend.material.color = col;
+        rend.material.shader = Shader.Find("Transparent/Diffuse");
+    }
+
+    bool getRandomBool() {
+        return (Random.Range(0, 2) > .5f);
+    }
+}
